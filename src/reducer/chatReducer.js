b0ac7c8461fd61e89bd3b6991a233eefaa0ef547
erwan <erwan.in.berlin@gmx.de>
2020-01-29 13:50:43 +0100

    deleted some console log lines

--- src/reducer/chatReducer.js
+++ src/reducer/chatReducer.js
@@ -20,42 +20,22 @@ const initialState = {
 
 const chatReducer = (state = initialState, action) => {
   if (action.type === 'ADD_COMMENT') {
-    console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-    console.log(action.payload);
 
     const newLine = [{
           text: action.payload.text,
           user: action.payload.user,
           id: new Date().getTime()
         }];
-    console.log("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy");
 
     let newArr = state.comments;
     let fin= newLine.concat(newArr);
 
-    console.log(fin)
-    
-    return Object.assign({}, {comments: fin});
+    return {comments:fin}
 
     
     
   }
 
-//   if (action.type === 'ADD_TODO') {
-// //     const newItem = {
-// //       status: false,
-// //       _id: new Date().getTime(),
-// //       text: action.payload,
-// //       date: '2020-01-26T20:52:04.184Z',
-// //       __v: 0
-// //     };
-
-// //     state.items.push(newItem);
-// //     return Object.assign({}, state);
-// //   }
-//       return state
-// //   return { ...state };
-//   }
   return { ...state };
 };
 
